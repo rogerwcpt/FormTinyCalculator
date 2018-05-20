ed54716440b3bae6e241ce5a3637c5b4ef98942b
Roger Weiss <roger.d.weiss@gmail.com>
2018-05-20 21:48:29 +0200

    Finished caclulator

--- FormsTinyCalculator/Main.cs
+++ FormsTinyCalculator/Main.cs
@@ -1,15 +1,14 @@
 ﻿using Xamarin.Forms;
-using System.Windows.Input;
+using System;
+using System.Linq;
 
 namespace FormsTinyCalculator {
+	
 	public partial class App : Application {
-		public App() {
-			InitializeComponent();
-			MainPage = new MainPage();
-		}
+		public App() { MainPage = new MainPage();}
 	}
 
-	public enum ButtonType { Digit, Operand, Cancel, Total };
+	public enum ButtonType { Digit, Operand, Cancel, Total};
 
 	public class GridButton: Button {
 		private Color[] Colors = new[] { Color.White, Color.Orange, Color.Gray, Color.Orange };
@@ -40,59 +39,61 @@ namespace FormsTinyCalculator {
 
 	public class MainPage : ContentPage {
 		private Label _displayLabel = new DisplayLabel();
+		private string _operand = null;
 
 		private (string, ButtonType, int, int, int)[] _buttonStruct = new[] {
 			("7", ButtonType.Digit, 1, 0, 1), ("8", ButtonType.Digit, 1, 1, 1), ("9", ButtonType.Digit, 1, 2, 1), ("/", ButtonType.Operand, 1, 3, 1),
 			("4", ButtonType.Digit, 2, 0, 1), ("5", ButtonType.Digit, 2, 1, 1), ("6", ButtonType.Digit, 2, 2, 1), ("x", ButtonType.Operand, 2, 3, 1),
 			("1", ButtonType.Digit, 3, 0, 1), ("2", ButtonType.Digit, 3, 1, 1), ("3", ButtonType.Digit, 3, 2, 1), ("-", ButtonType.Operand, 3, 3, 1),
 			("0", ButtonType.Digit, 4, 0, 3), ("+", ButtonType.Operand, 4, 3, 1),
-			("C", ButtonType.Cancel, 5, 0, 1), ("+", ButtonType.Total, 5, 1, 3)
+			("C", ButtonType.Cancel, 5, 0, 1), ("=", ButtonType.Total, 5, 1, 3)
 		};
+		private int _digit1, _digit2;
 
 		public MainPage() {
 			var grid = new Grid() { BackgroundColor = Color.Black, RowSpacing = 1, ColumnSpacing = 1 };
 			grid.Children.Add(_displayLabel);
 			foreach(var b in _buttonStruct) { grid.Children.Add(new GridButton(b.Item1, b.Item2, b.Item3, b.Item4, b.Item5));}
 			Content = grid;
-			BindingContext = this;
-		}
-	}
 
-	public class MainPageViewModel
-	{
-		private int _digit1, _digit2;
-		private string _operand;
-		private bool operandSet = false;
-
-		public MainPageViewModel() {
-			DigitCommand = new Command<string>(DoDigitCommand);
-			OperandCommand = new Command<string>(DoOperandCommand);
-			CancelCommand = new Command(DoCancelCommand);
-			TotalCommand = new Command(DoTotalCommand);
-		}
-
-		private void DoOperandCommand(string operand) {
-			_operand = operand;
-		}
-
-		private void DoDigitCommand(string digit) {
-			if (operandSet) {
-				_digit2 = int.Parse(digit);
-			} else  {
-				_digit1  = int.Parse(digit);
+			foreach(GridButton gridButton in grid.Children.Skip(1)) {
+				if (gridButton != null) {
+					switch (gridButton.ButtonType){
+						case ButtonType.Digit: gridButton.Clicked += DigitClicked; break;
+						case ButtonType.Operand: gridButton.Clicked += OperandClicked; break;
+						case ButtonType.Cancel: gridButton.Clicked += CancelClicked; break;
+						case ButtonType.Total : gridButton.Clicked += TotalClicked; break;
+					}
+				}
 			}
 		}
 
-		private void DoTotalCommand() {
+		private void CancelClicked(object sender, EventArgs e) {
+			_displayLabel.Text = "0";
+			_operand = null;
 		}
 
-		private void DoCancelCommand() {
+		private void TotalClicked(object sender, EventArgs e) {
+			switch(_operand) {
+				case "+": _displayLabel.Text = (_digit1 + _digit2).ToString(); break;
+				case "-": _displayLabel.Text = (_digit1 - _digit2).ToString(); break;
+				case "*": _displayLabel.Text = (_digit1 * _digit2).ToString(); break;
+				case "/": _displayLabel.Text = (_digit1 / _digit2).ToString(); break;
+			}
 		}
 
-		public ICommand DigitCommand { get; }
-		public ICommand OperandCommand { get; }
-		public ICommand TotalCommand { get; }
-		public ICommand CancelCommand { get; }
-		public string Total { get; set;}
+		private void OperandClicked(object sender, EventArgs e) {
+			_operand = ((Button)sender).Text;
+		}
+
+		void DigitClicked(object sender, System.EventArgs e) {
+			var text = ((Button)sender).Text;
+			if (string.IsNullOrEmpty(_operand)) {
+				_digit1 = int.Parse(text);
+			} else {
+				_digit2 = int.Parse(text);
+			}
+			_displayLabel.Text = text;
+		}
 	}
 }
