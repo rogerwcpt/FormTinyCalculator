0341870bd896989b59fc77f8a5bad17d3f65fb41
Roger Weiss <roger.d.weiss@gmail.com>
2018-05-20 17:32:06 +0200

    Initial Commit

---
+++ FormsTinyCalculator/App.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsTinyCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ FormsTinyCalculator/MainPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Net.Sockets;
+namespace FormsTinyCalculator
+{
+	public enum Operator { Add, Subtract, Multiply, Divide }
+
+	public class MainPage: ContentPage
+    {
+        public MainPage()
+        {
+			
+        }
+    }
+}
