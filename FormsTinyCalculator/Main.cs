d80b5692ba90f02daa7cba22f40bfb2860272c72
Roger Weiss <roger.d.weiss@gmail.com>
2018-06-02 19:28:18 +0200

    Replaced Blind Button casts with safe casts

--- FormsTinyCalculator/Main.cs
+++ FormsTinyCalculator/Main.cs
@@ -43,7 +43,7 @@ namespace FormsTinyCalculator {
 		private Label _displayLabel = new DisplayLabel();
 		private string _expression, _digits;
 
-		private (string, ButtonType, int, int, int)[] _buttonStruct = new[] {
+		private (string FirstName, ButtonType ButtonType, int Row, int Col, int Span)[] _buttonStruct = new[] {
 			("7", ButtonType.Digit, 1, 0, 1), ("8", ButtonType.Digit, 1, 1, 1), ("9", ButtonType.Digit, 1, 2, 1), ("/", ButtonType.Operand, 1, 3, 1),
 			("4", ButtonType.Digit, 2, 0, 1), ("5", ButtonType.Digit, 2, 1, 1), ("6", ButtonType.Digit, 2, 2, 1), ("*", ButtonType.Operand, 2, 3, 1),
 			("1", ButtonType.Digit, 3, 0, 1), ("2", ButtonType.Digit, 3, 1, 1), ("3", ButtonType.Digit, 3, 2, 1), ("-", ButtonType.Operand, 3, 3, 1),
@@ -54,17 +54,15 @@ namespace FormsTinyCalculator {
 		public MainPage() {
 			var grid = new Grid() { BackgroundColor = Color.Black, RowSpacing = 1, ColumnSpacing = 1 };
 			grid.Children.Add(_displayLabel);
-			foreach(var b in _buttonStruct) { grid.Children.Add(new GridButton(b.Item1, b.Item2, b.Item3, b.Item4, b.Item5));}
+			foreach(var b in _buttonStruct) { grid.Children.Add(new GridButton(b.FirstName, b.ButtonType, b.Row, b.Col, b.Span));}
 			Content = grid;
 
-			foreach(GridButton gridButton in grid.Children.Skip(1)) {
-				if (gridButton != null) {
-					switch (gridButton.ButtonType){
-						case ButtonType.Digit: gridButton.Clicked += DigitClicked; break;
-						case ButtonType.Operand: gridButton.Clicked += OperandClicked; break;
-						case ButtonType.Cancel: gridButton.Clicked += CancelClicked; break;
-						case ButtonType.Total : gridButton.Clicked += TotalClicked; break;
-					}
+			foreach(GridButton gridButton in grid.Children.Where(x => x is GridButton)) {
+				switch (gridButton.ButtonType){
+					case ButtonType.Digit: gridButton.Clicked += DigitClicked; break;
+					case ButtonType.Operand: gridButton.Clicked += OperandClicked; break;
+					case ButtonType.Cancel: gridButton.Clicked += CancelClicked; break;
+					case ButtonType.Total : gridButton.Clicked += TotalClicked; break;
 				}
 			}
 		}
@@ -77,19 +75,19 @@ namespace FormsTinyCalculator {
 
 		private void TotalClicked(object sender, EventArgs e) {
 			_displayLabel.Text = Convert.ToDouble(new DataTable().Compute(_expression, null)).ToString();
-			_digits = null;
 			_expression = null;
+			_digits = null;
 		}
 
 		private void OperandClicked(object sender, EventArgs e) {
-			_expression += ((Button)sender).Text;
+			_expression += (sender as Button)?.Text;
 			_digits = null;
 		}
 
 		private void DigitClicked(object sender, EventArgs e) {
-			_digits += ((Button)sender).Text;
 			_displayLabel.Text = _digits;
-			_expression += ((Button)sender).Text;
+			_expression += (sender as Button)?.Text;
+			_digits += (sender as Button)?.Text;
 		}
 	}
 }
