98ee795d2e8507744e44519a21ead9b67dc5f6b2
Roger Weiss <roger.d.weiss@gmail.com>
2018-06-03 10:34:52 +0200

    Bugfix for continuous calculation

--- FormsTinyCalculator/Main.cs
+++ FormsTinyCalculator/Main.cs
@@ -40,7 +40,6 @@ namespace FormsTinyCalculator {
 	public class MainPage : ContentPage {
 		private Label _displayLabel = new DisplayLabel();
 		private string _expression, _digits;
-
 		private (string FirstName, ButtonType ButtonType, int Row, int Col, int Span)[] _buttonStruct = new[] {
 			("7", ButtonType.Digit, 1, 0, 1), ("8", ButtonType.Digit, 1, 1, 1), ("9", ButtonType.Digit, 1, 2, 1), ("/", ButtonType.Operand, 1, 3, 1),
 			("4", ButtonType.Digit, 2, 0, 1), ("5", ButtonType.Digit, 2, 1, 1), ("6", ButtonType.Digit, 2, 2, 1), ("*", ButtonType.Operand, 2, 3, 1),
@@ -67,13 +66,12 @@ namespace FormsTinyCalculator {
 
 		private void CancelClicked(object sender, EventArgs e) {
 			_displayLabel.Text = "0";
-			_expression = null;
-			_digits = null;
+			_expression = _digits = null;
 		}
 
 		private void TotalClicked(object sender, EventArgs e) {
-			_displayLabel.Text = Convert.ToDouble(new DataTable().Compute(_expression, null)).ToString();
-			_expression = null;
+			_expression = Convert.ToDouble(new DataTable().Compute(_expression, null)).ToString();
+			_displayLabel.Text = _expression;
 			_digits = null;
 		}
 
