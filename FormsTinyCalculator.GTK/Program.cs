79325bcaf299ad5179338de8857a64953fcd261c
Roger Weiss <roger.d.weiss@gmail.com>
2018-12-20 11:02:57 +0200

    Added GTK Client Project

---
+++ FormsTinyCalculator.GTK/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Gtk;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace FormsTinyCalculator.GTK {
+	class MainClass {
+
+		[STAThread]
+		public static void Main(string[] args) {
+
+			Gtk.Application.Init();
+			Xamarin.Forms.Forms.Init();
+
+			var app = new FormsTinyCalculator.App();
+
+			var window = new FormsWindow();
+			window.LoadApplication(app);
+			window.SetApplicationTitle("Calculator");
+			window.Show();
+
+			Gtk.Application.Run();
+		}
+	}
+}
