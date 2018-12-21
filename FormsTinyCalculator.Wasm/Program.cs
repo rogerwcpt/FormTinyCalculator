8418a152cb55ecf693575057bfb442a35ddac671
Roger Weiss <roger.d.weiss@gmail.com>
2018-12-21 18:30:56 +0200

    Added Wasm Client

---
+++ FormsTinyCalculator.Wasm/Program.cs
@@ -0,0 +1,16 @@
+﻿using Ooui;
+using Xamarin.Forms;
+
+namespace FormsTinyCalculator.Wasm
+{
+    class Program
+    {
+ 		static void Main(string[] args) {
+            Forms.Init ();
+
+			var page = new MainPage();
+
+			UI.Publish("/", page.GetOouiElement());
+        }
+    }
+}
